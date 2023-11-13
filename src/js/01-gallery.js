410617fc071e8709ec5d26c7b157e9aa8825ece1
Vova <v.borysiuk91@gmail.com>
2023-11-13 22:04:12 +0200

    firt task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,34 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-//console.log(galleryItems);
-console.log("Vova")
+
+const galleryList = document.querySelector('.gallery');
+galleryList.insertAdjacentHTML('beforeend', doGalleryList(galleryItems));
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  animationSpeed: 250,
+});
+
+function doGalleryList(arrGalleryItems) {
+  return arrGalleryItems
+    .map(({ preview, original, description }) => {
+      //const { preview, origin, description } = el;
+      return `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                alt="${description}"
+            />
+        </a>
+    </li>`;
+    })
+    .join('');
+}
